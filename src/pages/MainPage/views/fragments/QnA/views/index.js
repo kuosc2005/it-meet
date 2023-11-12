f8ae2ce7f820102b9ed6bc84ca204dcfecb2766b
s_a_j_a_g <spd.sajag@gmail.com>
2023-11-12 08:04:35 +0545

    stylize scrollbars

--- src/pages/MainPage/views/fragments/QnA/views/index.js
+++ src/pages/MainPage/views/fragments/QnA/views/index.js
@@ -1,41 +1,41 @@
-import React from "react";
-import FAQItem from "./components/FAQItem";
-import "./QnA.sass";
-import useFAQ from "../hooks/useFAQ";
+import React from 'react';
+import FAQItem from './components/FAQItem';
+import './QnA.sass';
+import useFAQ from '../hooks/useFAQ';
 
 export default function QnA() {
   const { qna } = useFAQ();
   return (
-    <div className="QnA margin-center">
-      <div className="wrapper box">
-        <div className="faq">
+    <div className='QnA margin-center'>
+      <div className='wrapper box'>
+        <div className='faq'>
           <h2>Frequently Asked Questions</h2>
           {qna.map((val, i) => (
             <FAQItem key={i} question={val.question} answer={val.answer} />
           ))}
         </div>
       </div>
-      <div className="haveQuestions box">
+      <div className='haveQuestions box'>
         <h2>Have questions? Let’s talk.</h2>
-        <div className="onei">
-          <div className="input">
+        <div className='onei'>
+          <div className='input'>
             <p>Full name*</p>
-            <input required placeholder="Type your name" />
+            <input required placeholder='Type your name' />
           </div>
-          <div className="input">
+          <div className='input'>
             <p>Your email address*</p>
             <input
-              color="secondary"
+              color='secondary'
               required
-              placeholder="Type your email address"
+              placeholder='Type your email address'
             />
           </div>
         </div>
-        <div className="input">
+        <div className='input'>
           <p>Your message*</p>
-          <input placeholder="Type your message" />
+          <input placeholder='Type your message' />
         </div>
-        <button className="submit">Submit</button>
+        <button className='submit'>Submit</button>
       </div>
     </div>
   );
