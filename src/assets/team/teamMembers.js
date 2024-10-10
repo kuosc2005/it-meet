d443807a7f573aae83e15da8b6d15abd0cca4e0d
Arekh Shrestha <sthaarekh@Arekhs-Macbook-Air.local>
2024-10-11 01:18:21 +0545

    Added responsiveness to the design

--- src/assets/team/teamMembers.js
+++ src/assets/team/teamMembers.js
@@ -1,19 +1,91 @@
-// teamMembers.js
-const teamMembers = [
+// teamMEMBER.js
+const teamMEMBER = [
     {
-      name: "AREH SHRESTHA",
-      department: "TECHNICAL TEAM",
-      image: '/src/assets/pics/arekh.png',
-    //   image: "../pics/arekh.png",
+        name: "Asim Shrestha",
+        department: "LEAD",
+        image: '/src/assets/team/asim.png',
+      },
+    {
+        name: "Shasankha Luitel",
+        department: "SUB LEAD",
+        image: '/src/assets/team/shasankha.png',
     },
     {
-        name: "LAWAN POUDYAL",
-        department: "TECHNICAL TEAM",
-      image: '/src/assets/pics/pp1.png',
-
+        name: "Yural Pokhrel",
+        department: "SUB LEAD",
+        image: '/src/assets/team/yural.png',
+      },
+    {
+      name: "James Bhattarai",
+      department: "SUB LEAD",
+      image: '/src/assets/team/james.png',
     },
-
+    {
+        name: "Aashish Adhikari",
+        department: "MEMBER",
+        image: '/src/assets/team/aashish.png',
+    },
+    {
+        name: "Arekh Shrestha",
+        department: "MEMBER",
+        image: '/src/assets/team/arekh.png',
+    },
+    {
+        name: "Ashwini Subedi",
+        department: "MEMBER",
+        image: '/src/assets/team/ashwini.png',
+    },
+    {
+        name: "Kiran Dahal",
+        department: "MEMBER",
+        image: '/src/assets/team/kiran.png',
+    },
+    {
+        name: "Lawan Poudyal",
+        department: "MEMBER",
+        image: '/src/assets/team/lawan.png',
+    },
+    {
+        name: "Prajwal Ghimire",
+        department: "MEMBER",
+        image: '/src/assets/team/prajwal.png',
+    },
+    {
+        name: "Pramisha Sapkota",
+        department: "MEMBER",
+        image: '/src/assets/team/pramisha.png',
+    },
+    {
+        name: "Sarbesh KC",
+        department: "MEMBER",
+        image: '/src/assets/team/sarbesh.png',
+    },
+    {
+        name: "Shriharsh Sharma",
+        department: "MEMBER",
+        image: '/src/assets/team/shriharsh.png',
+    },
+    {
+        name: "Suvesh Gurung",
+        department: "MEMBER",
+        image: '/src/assets/team/suvesh.png',
+    },
+    {
+        name: "Swoham Kayastha",
+        department: "MEMBER",
+        image: '/src/assets/team/swoham.png',
+    },
+    {
+        name: "Utsav Subedi",
+        department: "MEMBER",
+        image: '/src/assets/team/utsav.png',
+    },
+    {
+        name: "Hello World",
+        department: "MEMBER",
+        image: '/src/assets/team/hello.png',
+    },
   ];
   
-  export default teamMembers;
+  export default teamMEMBER;
   
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,59 +1,24 @@
-// import React from 'react'
-// import image from '../assets/pics/arekh.png'
-import shadowImage from '../assets/images/bgreen.webp'
-// import teammembers from '@/assets/pics/teamMembers'
-// const Team = () => {
-//   return (
-//     <div className='bg-[#171A23]'>
-//     <h1 className="text-center text-6xl pt-10 font-bold align-middle  bg-clip-text text-transparent bg-gradient-to-r from-[#369FFF] via-[#3BD0A3] to-[#14C58F]">Meet The Team</h1>
-//       <div className=" text-white p-5 sm:p-5 md:p-11 lg:p-24">
-//         <div className=" mx-auto">
-//           <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-
-//             <div className="item group relative" style={{ borderBottom: '2px solid', borderImageSource: 'linear-gradient(90deg, #369FFF 0%, #3BD0A3 64.4%, #14C58F 91.9%)', borderImageSlice: 1}}>
-//               <div className="flex items-left justify mb-4">
-//                 <svg width="20" height="24" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                   <path d="M10 0L20 5.7735V17.3205L10 23.094L0 17.3205L0 5.7735L10 0Z" fill="#05E2BD" />
-//                   <path d="M7.03927 9.75626C6.86227 8.93736 7.75872 8.30966 8.46769 8.75607L13.6313 12.0073C14.3193 12.4405 14.1763 13.4824 13.397 13.7142L11.5787 14.2551C11.3809 14.3139 11.2077 14.4358 11.0857 14.6022L10.0123 16.0655C9.53007 16.723 8.49748 16.5029 8.32522 15.7059L7.03927 9.75626Z" fill="#171A23"/>
-//                 </svg>
-//                 <span className="ml-2 mt-0.5 text-sm">TECHNICAL TEAM</span>
-//               </div>
-              
-//               <h2 className="text-4xl md:text-3xl lg:text-4xl font-bold mb-2 bg-clip-text text-transparent bg-gradient-to-r from-[#369FFF] via-[#3BD0A3] to-[#14C58F]">AREKH SHRESTHA</h2>
-//               <div className='h-64 overflow-hidden flex justify-center'>
-//                 <img src={shadowImage} className="absolute h-[400px] w-auto transform top-5 opacity-60 " />
-//                 <img src={image} className="h-[400px] w-auto transform filter grayscale group-hover:filter-none object-cover transition duration-300" />
-//               </div>
-//             </div>
-
-
-//           </div>
-//         </div>
-//     </div>
-//   </div>
-//   )
-// }
-
-
-// export default Team
-
 import React from 'react';
-import teamMembers from '@/assets/pics/teamMembers'; // Adjust the import path as needed
-
+import teamMembers from '@/assets/team/teamMembers'; // Adjust the import path as needed
+import shadowImage from '@/assets/images/bgreen.webp'
 const Team = () => {
   return (
     <div className='bg-[#171A23]'>
-      <h1 className="text-center text-6xl pt-10 font-bold align-middle bg-clip-text text-transparent bg-gradient-to-r from-[#369FFF] via-[#3BD0A3] to-[#14C58F]">
+      <h1 className="text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl pt-10 pb-5 lg:pb-0 font-bold align-middle bg-clip-text text-transparent bg-gradient-to-r from-[#369FFF] via-[#3BD0A3] to-[#14C58F]">
         Meet The Team
       </h1>
-      <div className="text-white p-5 sm:p-5 md:p-11 lg:p-24">
+      
+      <div className="text-white p-5 sm:p-5 md:p-11 lg:p-20">
+      <h1 className="text-left text-3xl sm:text-4xl md:text-5xl xl:text-6xl pb-5 font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#369FFF] via-[#3BD0A3] to-[#14C58F]">
+        Technical Team
+      </h1>
         <div className="mx-auto">
-          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
             
             {teamMembers.map((member, index) => (
               <div
                 key={index}
-                className="item group relative"
+                className="item group relative pt-5"
                 style={{
                   borderBottom: '2px solid',
                   borderImageSource: 'linear-gradient(90deg, #369FFF 0%, #3BD0A3 64.4%, #14C58F 91.9%)',
@@ -64,19 +29,19 @@ const Team = () => {
                     <path d="M10 0L20 5.7735V17.3205L10 23.094L0 17.3205L0 5.7735L10 0Z" fill="#05E2BD" />
                     <path d="M7.03927 9.75626C6.86227 8.93736 7.75872 8.30966 8.46769 8.75607L13.6313 12.0073C14.3193 12.4405 14.1763 13.4824 13.397 13.7142L11.5787 14.2551C11.3809 14.3139 11.2077 14.4358 11.0857 14.6022L10.0123 16.0655C9.53007 16.723 8.49748 16.5029 8.32522 15.7059L7.03927 9.75626Z" fill="#171A23"/>
                   </svg>
-                  <span className="ml-2 text-lg">{member.department}</span>
+                  <span className="ml-2 text-sm pt-[3px] sm:pt-0 md:text-lg lg:text-base">{member.department}</span>
                 </div>
 
-                <h2 className="text-3xl md:text-3xl lg:text-4xl font-bold mb-2 bg-clip-text text-transparent bg-gradient-to-r from-[#369FFF] via-[#3BD0A3] to-[#14C58F]">
+                <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-2 bg-clip-text text-transparent bg-gradient-to-r from-[#369FFF] via-[#3BD0A3] to-[#14C58F]">
                   {member.name.toUpperCase()}
                 </h2>
 
-                <div className='h-64 overflow-hidden flex justify-center'>
-                <img src={shadowImage} className="absolute h-[400px] w-auto transform top-5 opacity-60 " />
+                <div className='h-56 sm:h-60 lg:h-64 overflow-hidden flex justify-center'>
+                <img src={shadowImage} className="absolute h-[400px] w-auto transform top-5 opacity-60" />
                  
                   <img
                     src={member.image}
-                    className="h-[400px] w-auto transform filter grayscale group-hover:filter-none object-cover transition duration-300"
+                    className="h-[300px] sm:h-[350px] lg:h-[400px] w-auto transform filter grayscale group-hover:filter-none object-contain transition duration-300"
                     alt={member.name}
                   />
                 </div>
