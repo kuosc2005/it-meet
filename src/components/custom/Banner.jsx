4caadf3e622c40aa0b6e0197a50a5742456a1909
notashwinii <ashwinii.subedi@gmail.com>
2024-10-14 22:52:55 +0545

    feat: added banner in events page

--- src/components/custom/Banner.jsx
+++ src/components/custom/Banner.jsx
@@ -9,52 +9,37 @@ import EllipeBG from '@/assets/images/Ellipse.webp'
 const Banner = () => {
   return (
     <div className=" flex flex-col overflow-hidden p-10 py-20 lg:px-20 gap-8 items-center justify-center min-h-screen bg-[#171A23]">
-      
-      
       <div className="absolute bottom-0 left-5 z-10 w-6/12 max-w-3xl">
-      <div className="w-full h-24 sm:h-32 md:h-40 lg:h-48 xl:h-56 shadow-lg relative overflow-hidden">
-        <div 
-          className="w-full h-full absolute top-0 left-0"
-          style={{
-            background: 'linear-gradient(107.29deg, rgba(0, 147, 208,0.50) 5.8%, rgba(5, 226, 189,0.50) 86.5%)',
-            clipPath: 'polygon(2rem 0, 100% 0, 100% 100%, 0 100%, 0 2rem)',
-          }}
-        ></div>
-        <div className="relative z-10 flex items-center justify-center h-full">
-          <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-white">Events</h2>
+        <div className="w-full h-24 sm:h-32 md:h-40 lg:h-48 xl:h-56 shadow-lg relative overflow-hidden">
+          <div
+            className="w-full h-full absolute top-0 left-0"
+            style={{
+              background:
+                'linear-gradient(107.29deg, rgba(0, 147, 208,0.50) 5.8%, rgba(5, 226, 189,0.50) 86.5%)',
+              clipPath: 'polygon(2rem 0, 100% 0, 100% 100%, 0 100%, 0 2rem)',
+            }}
+          ></div>
+          <div className="relative z-10 flex items-center justify-center h-full">
+            <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-white">
+              Events
+            </h2>
+          </div>
         </div>
       </div>
-    </div>
 
-     
       <div className="absolute inset-0 pointer-events-none z-0">
-        <img
-          src={texture}
-          alt="texture"
-          className="w-full h-full object-cover"
-        />
+        <img src={texture} alt="texture" className="w-full h-full object-cover" />
         <div className="bg-pattern opacity-50 h-full w-full"></div>
       </div>
 
-      
       <div className="absolute inset-0 pointer-events-none z-5">
-        <img
-          src={Bg}
-          alt="background glow"
-          className="w-full h-full object-cover"
-        />
+        <img src={Bg} alt="background glow" className="w-full h-full object-cover" />
       </div>
 
-      
       <div className="absolute top-0 left-0 z-5">
-        <img
-          src={Blue}
-          alt="blue"
-          className="object-contain w-[50vw] max-w-[700px]"
-        />
+        <img src={Blue} alt="blue" className="object-contain w-[50vw] max-w-[700px]" />
       </div>
 
-      
       <div className="absolute bottom-0 right-0 z-5">
         <img
           src={EllipeBG}
@@ -63,7 +48,6 @@ const Banner = () => {
         />
       </div>
 
-     
       <div className="absolute bottom-0 right-0 z-10">
         <img
           src={Mascot}
@@ -72,7 +56,6 @@ const Banner = () => {
         />
       </div>
 
-     
       <div className="absolute bottom-0 right-0 z-10">
         <img
           src={BoundaryGreen}
