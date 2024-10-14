5c932e619fe18476628a948d66b25288af2971db
notashwinii <ashwinii.subedi@gmail.com>
2024-10-14 19:42:41 +0545

    feat: updated the events page with event banner

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,15 +1,21 @@
 import Expectation from '@/components/custom/Expectation'
 import MainEventPage from '@/components/custom/Main-eventPage'
 import PreEventPage from '@/components/custom/Pre-eventPage'
+import Banner from '@/components/custom/Banner'
+import EventList from '@/components/custom/EventList'
+
 import React from 'react'
 
 const Events = () => {
   return (
     <div className="flex flex-col overflow-hidden p-10 py-20 lg:px-20 gap-8 items-center justify-center min-h-screen bg-[#171A23]">
       <div className="space-y-4">
-        {/*Banner */}
         <div>
+          <Banner />
+        </div>
 
+        <div>
+          <EventList />
         </div>
         <div>
           <PreEventPage />
