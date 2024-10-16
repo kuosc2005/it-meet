c05258246708f5a32c7aa106993c2f5206f16b56
notashwinii <ashwinii.subedi@gmail.com>
2024-10-16 23:45:00 +0545

    feat: implement dynamic routing using event titles

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ import Prospectus from './pages/Prospectus'
 import Navbar from './components/custom/Navbar'
 import NotFoundPage from './pages/NotFoundPage'
 import Events from './pages/Events'
+import EventDetails from './pages/EventDetails'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Routes>
-        <Route path="/" element={<Home />} errorElement={<NotFoundPage />}/>
-        <Route path="/events" element={<Events />} errorElement={<NotFoundPage />}/>
-        <Route path="/teams" element={<Team />} errorElement={<NotFoundPage />}/>
-        <Route path="/prospectus" element={<Prospectus />} errorElement={<NotFoundPage />}/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} errorElement={<NotFoundPage />} />
+        <Route path="/events" element={<Events />} errorElement={<NotFoundPage />} />
+
+        <Route path="/events" element={<Events />} />
+        <Route path="/events/:eventTitle" element={<EventDetails />} />
+
+        <Route path="/teams" element={<Team />} errorElement={<NotFoundPage />} />
+        <Route path="/prospectus" element={<Prospectus />} errorElement={<NotFoundPage />} />
       </Routes>
       <Footer />
     </>
