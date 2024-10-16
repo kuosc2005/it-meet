a8743c46cc2720195add56afc05b3bd547dc3c2c
notashwinii <ashwinii.subedi@gmail.com>
2024-10-16 23:44:33 +0545

    fix: centralize event data in Events component

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,31 +1,47 @@
+import React from 'react'
 import Expectation from '@/components/custom/Expectation'
 import MainEventPage from '@/components/custom/Main-eventPage'
 import PreEventPage from '@/components/custom/Pre-eventPage'
 import Banner from '@/components/custom/Banner'
 import EventList from '@/components/custom/EventList'
 
-import React from 'react'
-
 const Events = () => {
+  const mainEvents = [
+    {
+      id: 1,
+      imgSrc: '',
+      title: 'KU HACKFEST',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ut labore et dolore magna aliqua.',
+      formLink: '',
+    },
+  ]
+
+  const preEvents = [
+    {
+      id: 1,
+      imgSrc: '',
+      title: 'KU HACKFEST',
+    },
+  ]
+
+  const listEvents = [
+    {
+      id: 1,
+      imgSrc: '',
+      title: 'KU HACKFEST',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+  ]
+
   return (
     <div className="flex flex-col overflow-hidden p-10 py-20 lg:px-20 gap-8 items-center justify-center min-h-screen bg-[#171A23]">
       <div className="space-y-4">
-        <div>
-          <Banner />
-        </div>
-
-        <div>
-          <EventList />
-        </div>
-        <div>
-          <PreEventPage />
-        </div>
-        <div>
-          <MainEventPage />
-        </div>
-        <div>
-          <Expectation />
-        </div>
+        <Banner />
+        <EventList events={listEvents} />
+        <PreEventPage events={preEvents} />
+        <MainEventPage events={mainEvents} />
       </div>
     </div>
   )
