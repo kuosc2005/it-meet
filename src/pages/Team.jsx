ce3eee40c65c17f7283b134838fa7004a70612d8
sthaarekh <arekhshrestha@gmail.com>
2024-10-18 22:22:32 +0545

    Added social media handle to the frame

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import teamMembers from '@/assets/team/teamMembers'; // Adjust the import path as needed
-import shadowImage from '@/assets/images/bgreen.webp'
+import shadowImage from '@/assets/images/bgimg.png'
+import { FaGithub } from 'react-icons/fa';
+import { GrLinkedinOption } from 'react-icons/gr';
+import { IoLogoInstagram } from "react-icons/io";
+
 const Team = () => {
   return (
     <div className='bg-[#171A23]'>
@@ -9,21 +13,15 @@ const Team = () => {
       </h1>
       
       <div className="text-white p-5 sm:p-5 md:p-11 lg:p-20:pt-0 ">
-      <h1 className="text-left text-3xl sm:text-4xl md:text-5xl xl:text-6xl pb-5 font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#369FFF] via-[#3BD0A3] to-[#14C58F]">
-        Technical Team
-      </h1>
-        <div className="mx-auto">
+        <h1 className="text-left text-3xl sm:text-4xl md:text-5xl xl:text-6xl pb-5 font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#369FFF] via-[#3BD0A3] to-[#14C58F]">
+          Technical Team
+        </h1>
+
+        <div className="p-3">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-5">
-            
             {teamMembers.map((member, index) => (
-              <div
-                key={index}
-                className="item group relative pt-5"
-                style={{
-                  borderBottom: '2px solid',
-                  borderImageSource: 'linear-gradient(90deg, #369FFF 0%, #3BD0A3 64.4%, #14C58F 91.9%)',
-                  borderImageSlice: 1
-                }}>
+              <div key={index} className="item group relative pt-5 border-b-2 border-b-[linear-gradient(90deg,_#369FFF_0%,_#3BD0A3_64.4%,_#14C58F_91.9%)]">
+
                 <div className="flex items-left justify mb-4">
                   <svg className="w-[15px] h-[24px] md:w-[20px]" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M10 0L20 5.7735V17.3205L10 23.094L0 17.3205L0 5.7735L10 0Z" fill="#05E2BD" />
@@ -36,15 +34,21 @@ const Team = () => {
                   {member.name.toUpperCase()}
                 </h2>
 
-                <div className='h-56 sm:h-60 lg:h-64 overflow-hidden flex justify-center'>
-                <img src={shadowImage} className="absolute h-[400px] w-auto transform top-5 opacity-60" />
-                 
-                  <img
-                    src={member.image}
-                    className="h-[250px] sm:h-[300px] lg:h-[350px] w-auto transform filter grayscale group-hover:filter-none object-contain transition duration-300"
-                    alt={member.name}
-                  />
+                <div className="h-56 flex sm:h-60 lg:h-64 overflow-hidden">
+                  <div className="flex flex-col items-center justify-center ml-2 mr-20 lg:mr-32 xl:mr-28">
+                      <GrLinkedinOption className="text-white bg-[#0077b5] rounded-sm mb-2 cursor-pointer" size={25} onClick={() => window.open(member.linkedin, '_blank')}/>
+                      <IoLogoInstagram className="bg-white bg-gradient-to-b from-[#833ab4] via-[#fd1d1d] to-[#fcb045] rounded-md mb-2 cursor-pointer" size={25} onClick={() => window.open(`https://instagram.com/${member.insta}`, '_blank')}/>
+                      <FaGithub className="cursor-pointer" color="white" size={25} onClick={() => window.open(`https://github.com/${member.github}`, '_blank')}/>
+                  </div>
+                  <div className='relative'>
+                    <img src={shadowImage} className="absolute w-[300px] blur-lg"/>
+                    <img src={member.image} className="h-[250px] sm:h-[300px] lg:h-[350px] w-auto transform filter grayscale group-hover:filter-none object-contain transition duration-300" alt={member.name}/>
+                  </div>
                 </div>
+
+
+
+
               </div>
             ))}
 
