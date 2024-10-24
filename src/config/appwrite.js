907c5819adcbf4e96e2514ee2037699983f14294
Aashish1-1-1 <aashishadhikari693@gmail.com>
2024-10-24 11:57:01 +0545

    [Update:appwrite.js]

--- src/config/appwrite.js
+++ src/config/appwrite.js
@@ -1,18 +1,20 @@
-import { Client, Databases, Storage } from "appwrite";
+import { Client, Databases, Storage } from 'appwrite'
 
-export const client = new Client();
+export const client = new Client()
 
 client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID)
 
-export const DATABASE_ID = "ITMeetDatabase";
-export const PROJECT_BUCKET_ID = "671605330012c01f3505"
+export const DATABASE_ID = 'ITMeetDatabase'
+export const PROJECT_BUCKET_ID = '671605330012c01f3505'
 
-export const FAQ_COLLECTION_ID = "FAQ";
-export const EXPECT_COLLECTION_ID = "Expectation";
+export const FAQ_COLLECTION_ID = 'FAQ'
+export const EXPECT_COLLECTION_ID = 'Expectation'
 
-export const databases = new Databases(client);
-export const storage = new Storage(client);
+export const Sponsors_COLLECTION_ID = 'Sponsor'
 
-export { ID } from "appwrite";
+export const databases = new Databases(client)
+export const storage = new Storage(client)
+
+export { ID } from 'appwrite'
