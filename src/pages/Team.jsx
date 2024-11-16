4a75e4adbb013a11a1afeb5992f423434e11afa5
sthaarekh <arekhshrestha@gmail.com>
2024-11-16 10:48:12 +0545

    Added arrow button for mobile view

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -22,25 +22,44 @@ const Team = () => {
         <div className="text-white pl-8 pr-8 sm:pl-11 sm:pr-11 md:pl-20 md:pr-20 lg:pl-24 lg:pr-24">
         {/* For mobile screen */}
         <div className="sm:hidden">
-          <button className="w-full px-4 py-2 bg-gray-700 text-gray-300 font-semibold rounded hover:bg-gradient-to-r hover:from-[#369FFF] hover:to-[#14C58F] hover:text-white" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+          <button
+            className="w-full flex items-center justify-between px-4 py-2 bg-gray-700 text-gray-300 font-semibold rounded hover:bg-gradient-to-r hover:from-[#369FFF] hover:to-[#14C58F] hover:text-white"
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          >
             {team[selectedTeamIndex]?.title || team[0]?.title}
+            {/* Arrow SVG */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-5 w-5 transition-transform duration-300 ${
+                isDropdownOpen ? "rotate-180" : ""
+              }`}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+            </svg>
           </button>
           {isDropdownOpen && (
             <div className="bg-gray-800 rounded shadow-lg">
               {team.map((team, index) => (
                 <button key={index} className={`w-full text-left px-4 py-2 text-[12px] font-semibold transition-all duration-300 ease-in-out ${ index === selectedTeamIndex
                       ? "bg-gradient-to-r from-[#369FFF] to-[#14C58F] text-white"
-                      : "bg-gray-700 text-gray-300 hover:bg-gradient-to-r hover:from-[#369FFF] hover:to-[#14C58F] hover:text-white"}`}
+                      : "bg-gray-700 text-gray-300 hover:bg-gradient-to-r hover:from-[#369FFF] hover:to-[#14C58F] hover:text-white"
+                  }`}
                   onClick={() => {
                     handleTeamClick(index);
                     setIsDropdownOpen(false); // Close dropdown after selection
-                  }}>
+                  }}
+                >
                   {team.title}
                 </button>
               ))}
             </div>
-          )}
-        </div>
+            )}
+          </div>
+
 
       {/* Grid layout for larger screens */}
       <div className="hidden sm:grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4 mb-8">
